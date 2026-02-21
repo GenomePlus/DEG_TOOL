5f0abb4a36367770af604c809b6de6885cf19585
GenomePlus <genomeplus.phoenixbioinfosys@gmail.com>
2026-02-21 19:53:05 +0530

    Update mirna_analysis.py

--- mirna_analysis.py
+++ mirna_analysis.py
@@ -1,10 +1,5 @@
-import networkx as nx
+from database_connector import run_enrichr
 
-def build_mirna_network(mirna_df, deg_genes):
-    filtered = mirna_df[mirna_df["TargetGene"].isin(deg_genes)]
-    G = nx.Graph()
-
-    for _, row in filtered.iterrows():
-        G.add_edge(row["miRNA"], row["TargetGene"])
-
-    return G, filtered
+def analyze_mirna(gene_list):
+    library = "miRTarBase_2017"
+    return run_enrichr(gene_list, library)
